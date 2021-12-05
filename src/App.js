7c06875b090a22485ca9860b4e7cf880f23b7e04
Mainul islam <mainulislam163@gmail.com>
2021-12-06 00:28:19 +0600

    sass and antd css installl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Row, Col } from "antd";
+import Home from "./container/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
